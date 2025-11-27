bc4db94f848a1d2b8a225354c17a8de907528744
DennisNKamau <njaudennis@gmail.com>
2025-11-28 00:20:12 +0300

    Update Skills.js

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -60,7 +60,7 @@ const Skills = () => {
           </div>
           {/* Hindi */}
           <div className="py-3 border-b-[1px] border-zinc-800">
-            <p className="text-base text-textColor -mb-1.5">Hindi</p>
+            <p className="text-base text-textColor -mb-1.5">Swahili</p>
             <span className="w-full bg-zinc-600 h-1 inline-flex relative">
               <span className="w-[90%] h-full absolute top-0 left-0 bg-designColor"></span>
             </span>
